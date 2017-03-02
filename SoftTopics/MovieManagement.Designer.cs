461b3bbcf2f597c3d66d1d2dc67b721daed1ef28
bcrumrin <bcrum3@uis.edu>
2017-03-02 14:48:03 -0600

    Barcodes
    Generates barcodes when adding movies, and sends to default printer

--- SoftTopics/MovieManagement.Designer.cs
+++ SoftTopics/MovieManagement.Designer.cs
@@ -29,13 +29,17 @@
         private void InitializeComponent()
         {
             this.lvMovies = new System.Windows.Forms.ListView();
+            this.MovieTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Barcode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.txtBarcode = new System.Windows.Forms.TextBox();
             this.txtTitle = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnAddMovie = new System.Windows.Forms.Button();
-            this.MovieTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Barcode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblBarcode = new System.Windows.Forms.Label();
+            this.txtCopyNumber = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCopyNumber)).BeginInit();
             this.SuspendLayout();
             // 
             // lvMovies
@@ -49,50 +53,6 @@
             this.lvMovies.TabIndex = 0;
             this.lvMovies.UseCompatibleStateImageBehavior = false;
             // 
-            // txtBarcode
-            // 
-            this.txtBarcode.Location = new System.Drawing.Point(338, 104);
-            this.txtBarcode.MaxLength = 12;
-            this.txtBarcode.Name = "txtBarcode";
-            this.txtBarcode.Size = new System.Drawing.Size(100, 20);
-            this.txtBarcode.TabIndex = 1;
-            // 
-            // txtTitle
-            // 
-            this.txtTitle.Location = new System.Drawing.Point(338, 130);
-            this.txtTitle.MaxLength = 32;
-            this.txtTitle.Name = "txtTitle";
-            this.txtTitle.Size = new System.Drawing.Size(100, 20);
-            this.txtTitle.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(279, 107);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Barcode: ";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(267, 133);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Movie Title: ";
-            // 
-            // btnAddMovie
-            // 
-            this.btnAddMovie.Location = new System.Drawing.Point(363, 156);
-            this.btnAddMovie.Name = "btnAddMovie";
-            this.btnAddMovie.Size = new System.Drawing.Size(75, 23);
-            this.btnAddMovie.TabIndex = 5;
-            this.btnAddMovie.Text = "Add Movie";
-            this.btnAddMovie.UseVisualStyleBackColor = true;
-            this.btnAddMovie.Click += new System.EventHandler(this.btnAddMovie_Click);
-            // 
             // MovieTitle
             // 
             this.MovieTitle.Text = "MovieTitle";
@@ -103,11 +63,89 @@
             this.Barcode.Text = "Barcode";
             this.Barcode.Width = 100;
             // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(384, 95);
+            this.txtBarcode.MaxLength = 10;
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(100, 20);
+            this.txtBarcode.TabIndex = 1;
+            this.txtBarcode.TextChanged += new System.EventHandler(this.txtBarcode_TextChanged);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(384, 121);
+            this.txtTitle.MaxLength = 32;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Barcode: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Movie Title: ";
+            // 
+            // btnAddMovie
+            // 
+            this.btnAddMovie.Location = new System.Drawing.Point(409, 184);
+            this.btnAddMovie.Name = "btnAddMovie";
+            this.btnAddMovie.Size = new System.Drawing.Size(75, 23);
+            this.btnAddMovie.TabIndex = 5;
+            this.btnAddMovie.Text = "Add Movie";
+            this.btnAddMovie.UseVisualStyleBackColor = true;
+            this.btnAddMovie.Click += new System.EventHandler(this.btnAddMovie_Click);
+            // 
+            // lblBarcode
+            // 
+            this.lblBarcode.AutoSize = true;
+            this.lblBarcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBarcode.Location = new System.Drawing.Point(262, 242);
+            this.lblBarcode.Name = "lblBarcode";
+            this.lblBarcode.Size = new System.Drawing.Size(0, 51);
+            this.lblBarcode.TabIndex = 6;
+            // 
+            // txtCopyNumber
+            // 
+            this.txtCopyNumber.Location = new System.Drawing.Point(384, 147);
+            this.txtCopyNumber.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.txtCopyNumber.Name = "txtCopyNumber";
+            this.txtCopyNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtCopyNumber.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(281, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Number of Copies: ";
+            // 
             // MovieManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 399);
+            this.ClientSize = new System.Drawing.Size(536, 399);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCopyNumber);
+            this.Controls.Add(this.lblBarcode);
             this.Controls.Add(this.btnAddMovie);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -117,6 +155,7 @@
             this.Name = "MovieManagement";
             this.Text = "MovieManagement";
             this.Load += new System.EventHandler(this.MovieManagement_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtCopyNumber)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,5 +171,8 @@
         private System.Windows.Forms.Button btnAddMovie;
         private System.Windows.Forms.ColumnHeader MovieTitle;
         private System.Windows.Forms.ColumnHeader Barcode;
+        private System.Windows.Forms.Label lblBarcode;
+        private System.Windows.Forms.NumericUpDown txtCopyNumber;
+        private System.Windows.Forms.Label label3;
     }
 }
--- SoftTopics/MovieManagement.cs
+++ SoftTopics/MovieManagement.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
+using System.Drawing.Printing;
 
 namespace SoftTopics
 {
     public partial class MovieManagement : Form
     {
+        Font myFont;
+        StreamReader fileToPrint;
         public MovieManagement()
         {
+
             InitializeComponent();
         }
 
         private void MovieManagement_Load(object sender, EventArgs e)
         {
+            loadFont();
             string movieFile = "..\\Files\\MovieList.txt";
             using (StreamReader sr = new StreamReader(movieFile))
             {
@@ -38,6 +44,16 @@ namespace SoftTopics
             }
         }
 
+        private void loadFont()
+        {
+            PrivateFontCollection font = new PrivateFontCollection();
+            //Font myFont;
+
+            font.AddFontFile("..\\Files\\BarcodeFont.ttf");
+            myFont = new Font(font.Families[0], 85);
+            lblBarcode.Font = myFont;
+        }
+
         private void btnAddMovie_Click(object sender, EventArgs e)
         {
 
@@ -49,24 +65,85 @@ namespace SoftTopics
                     MessageBox.Show("There is already a movie with that barcode", "error");
                 }
             }
-            if (txtTitle.Text.Length != 0 && txtBarcode.Text.Length == 12)
+            if (txtTitle.Text.Length != 0 && txtBarcode.Text.Length == 10 && txtCopyNumber.Value > 0)
             {
-                
+                int count = (int)txtCopyNumber.Value;
                 string movieFile = "..\\Files\\MovieList.txt";
                 using (StreamWriter sw = File.AppendText(movieFile))
                 {
-                    string title = txtTitle.Text;
-                    string barcode = txtBarcode.Text;
-                    string newLine = barcode + "," + title + "," + "3";
-                    newLine = newLine.Replace(" ", "");
-                    sw.WriteLine(newLine);
+                    using (StreamWriter swTemp = new StreamWriter("..\\Files\\temp.txt"))
+                    {
 
-                    string[] newItem = { title, barcode };
-                    lvMovies.View = View.Details;
-                    lvMovies.FullRowSelect = true;
-                    lvMovies.Items.Add(new ListViewItem(newItem));
+
+                        while (count > 0)
+                        {
+                            string copyNumber = count.ToString("D2");
+
+                            string title = txtTitle.Text;
+                            string barcode = txtBarcode.Text + copyNumber;
+                            string newLine = barcode + "," + title;
+                            newLine = newLine.Replace(" ", "");
+                            sw.WriteLine(newLine);
+
+                            string[] newItem = { title, barcode };
+                            lvMovies.View = View.Details;
+                            lvMovies.FullRowSelect = true;
+                            lvMovies.Items.Add(new ListViewItem(newItem));
+
+                            swTemp.WriteLine(barcode);
+
+                            count--;
+                        }
+                    }
                 }
-                
+
+
+                print();
+            }
+        }
+
+        private void print()
+        {
+            fileToPrint = new StreamReader("..\\Files\\temp.txt");
+
+            PrintDocument print = new PrintDocument();
+            print.PrintPage += new PrintPageEventHandler(this.print_PrintPage);
+            print.Print();
+            fileToPrint.Close();
+
+        }
+
+        private void print_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font printFont = myFont;
+            float yPos = 0f;
+            int count = 0;
+            float leftMargin = e.MarginBounds.Left;
+            float topMargin = e.MarginBounds.Top;
+            string line = null;
+            float linesPerPage = e.MarginBounds.Height / printFont.GetHeight(e.Graphics);
+            while (count < linesPerPage)
+            {
+                line = fileToPrint.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                yPos = topMargin + count * printFont.GetHeight(e.Graphics);
+                e.Graphics.DrawString(line, printFont, Brushes.Black, leftMargin, yPos, new StringFormat());
+                count++;
+            }
+            if (line != null)
+            {
+                e.HasMorePages = true;
+            }
+        }
+
+        private void txtBarcode_TextChanged(object sender, EventArgs e)
+        {
+            if (txtBarcode.Text.Length == 10)
+            {
+                lblBarcode.Text = txtBarcode.Text;
             }
         }
     }
